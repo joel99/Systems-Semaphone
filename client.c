0461ca02a56ef08810b761cc0a338bf42a363241
Joel Ye <jye5@stuy.edu>
2016-12-08 15:35:27 -0500

    ok

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <string.h>
+
+int main(int argc, char * argv[]){
+
+  int fd = open("story.txt", O_CREAT | O _RDWR)
+
+}
